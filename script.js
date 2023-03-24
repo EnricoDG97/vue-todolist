52c9c23aee8dcf635c289d31ea5b3541a99e4572
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-03-24 15:28:31 +0100

    feat: added vue.js function via html

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
 const { createApp } = Vue;
-
 createApp({
     data() {
         return {
-
+            toDoElements: [
+                {
+                    text: 'Fare X',
+                    done: true
+                },
+                {
+                    text: 'Disfare Y',
+                    done: false
+                },
+                {
+                    text: 'Sentire Z',
+                    done: true
+                },
+                {
+                    text: 'Trovare Q',
+                    done: true
+                },
+                {
+                    text: 'Prendere K',
+                    done: false
+                },
+            ],
+    
+            newToDo: {
+                text: '',
+                done: false
+            },
         }
-    }
+    },
 
-}).mount('#app')
+    methods: {
+        selectToDo(toDo) {
+            console.log(toDo);
+            if(toDo.done == true) {
+                toDo.done = false;
+            } else {
+                toDo.done = true;
+            }
+        },
+    
+        deleteToDo(toDoIndex) {
+            this.toDoElements.splice(toDoIndex, 1);
+        },
+
+        addToDo() {
+            if(this.newToDo.text == "") {
+                alert("Attenzione, To Do incompleto!");
+            } else {
+                this.toDoElements.push(this.newToDo);
+                this.newToDo = {
+                    text: "",
+                    done: false
+                }
+            }
+        }
+    },
+}).mount('#app');
