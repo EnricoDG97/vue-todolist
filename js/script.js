10d0bd18f917c1a4fe17d8da262b300ba805c869
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-11-15 16:30:45 +0100

    feat: added initial todos and functions to add more

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: "ciao"
+            toDos: [
+                "Creare repository",
+                "Aprire VStudio",
+                "Clonare la repository"
+            ],
+            newToDo: "",
+        };
+    },
+    methods: {
+        addNewToDo: function() {
+            this.toDos.push(this.newToDo);
         }
     }
 }).mount('#app');
